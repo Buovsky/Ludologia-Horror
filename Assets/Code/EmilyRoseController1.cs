5e7c08b65791a12f6397a3191859b9b2a10b497d
Buovsky <57236090+Buovsky@users.noreply.github.com>
2020-11-11 23:36:09 +0100

    Created various of scripts, pimped light, added fiew audio clips

---
+++ Assets/Code/EmilyRoseController1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class EmilyRoseController1: MonoBehaviour {
+
+	IEnumerator Start() {
+		yield return null;
+		GetComponent<Animator>().enabled = false;
+	}
+
+	[Button]
+	public void LaunchEmily() {
+		StartCoroutine(LaunchEmilyCoroutine());
+	}
+
+	IEnumerator LaunchEmilyCoroutine() {
+		GetComponent<Animator>().enabled = true;
+		yield return new WaitForSeconds(1.2f);
+		//Darkener.instance.ShockThePlayer();
+		//yield return new WaitForSeconds(0.4f);
+        //gameObject.SetActive(false);
+        GetComponent<Animator>().enabled = false;
+    }
+}
---
+++ Assets/Code/EmilyRoseController2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class EmilyRoseController2: MonoBehaviour {
+
+	IEnumerator Start() {
+		yield return null;
+		GetComponent<Animator>().enabled = false;
+	}
+
+	[Button]
+	public void LaunchEmily() {
+		StartCoroutine(LaunchEmilyCoroutine());
+	}
+
+	IEnumerator LaunchEmilyCoroutine() {
+		//GetComponent<Animator>().enabled = true;
+		yield return new WaitForSeconds(4.2f);
+		Darkener.instance.ShockThePlayer();
+		//yield return new WaitForSeconds(0.4f);
+        //gameObject.SetActive(false);
+        //GetComponent<Animator>().enabled = false;
+    }
+}
